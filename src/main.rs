566dee147cb8eab68bcae04527a307f35e4c8821
wallacelw <70167391+wallacelw@users.noreply.github.com>
2023-01-31 16:02:26 -0300

    all 4 moves

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,20 @@ fn app() -> Html {
 }
 
 /*
-Função para gerar um novo bloco em uma posição livre ( =0 ) com
+Função que verifica se o jogo acabou, ou seja, não existe mais nenhum espaço vazio e nenhum conjunto de blocos podem ser unidos.
+*/
+fn check_end(matrix : &mut [[u128; 4]; 4]) -> bool {
+    return true;
+}
+
+/*
+Função para gerar um novo bloco em uma posição livre (=0) com
 a probabilidade de 90% de gerar um bloco de valor 2 e probabilidade
 de 10% de gerar um bloco de valor 4. A posição é escolhida de modo equiprovável.
+Retorna um valor booleano indicando se o jogo acabou.
+true -> acabou; false -> jogo segue
 */
-fn generate_tile(matrix : &mut [[u128; 4]; 4]) {
+fn generate_tile(matrix : &mut [[u128; 4]; 4]) -> bool {
 
     let mut zeros: usize = 0;
     for i in 0..4 {
@@ -22,9 +31,14 @@ fn generate_tile(matrix : &mut [[u128; 4]; 4]) {
         }
     }
 
+    // Asserts if there is at least 1 empty space for generation
+    assert!(!(zeros == 0), "Impossible game: no more zeros");
+    if zeros == 0 {return false};
+
     let mut position: usize = rand::thread_rng().gen_range(0..(zeros));
 
-    // value == 9 -> tile(4); value < 9 -> tile(2)
+    // value == 9 -> tile(4); 
+    // value < 9 -> tile(2)
     let value: u128 = if rand::thread_rng().gen_range(0..10) == 9 {4} else {2};
 
     for i in 0..4 {
@@ -32,7 +46,10 @@ fn generate_tile(matrix : &mut [[u128; 4]; 4]) {
             if matrix[i][j] == 0 {
                 if position == 0 {
                     matrix[i][j] = value;
-                    return;
+                    if zeros == 15 {
+                        return check_end(matrix);
+                    }
+                    return false;
                 }
                 else {
                     position -= 1;
@@ -40,6 +57,8 @@ fn generate_tile(matrix : &mut [[u128; 4]; 4]) {
             }
         }
     }
+
+    return false;
 }
 
 // debugging function
@@ -53,7 +72,8 @@ fn print_matrix(matrix : [[u128; 4]; 4]) {
     println!();
 }
 
-fn transverse(matrix : &mut [[u128; 4]; 4], direction : [u128; 2]) {
+// Movement of the game for the left arrow
+fn move_left(matrix : &mut [[u128; 4]; 4]) {
 
     let mut merged: [ [bool; 4]; 4] = [
         [false, false, false, false],
@@ -63,42 +83,145 @@ fn transverse(matrix : &mut [[u128; 4]; 4], direction : [u128; 2]) {
     ];
 
     for k in 0..3 {
-        for j in 1..4 {
-            for i in 0..(4-k) {
-                if matrix[i][j] == matrix[i][j-1] {
+        for j in 1..(4-k) {
+            for i in 0..4 {
+                if (matrix[i][j] == matrix[i][j-1]) && (!merged[i][j-1]) {
+                    matrix[i][j-1] *= 2;
+                    matrix[i][j] = 0;
 
-                    // merging with left cell
-                    if !merged[i][j-1] {
-                        matrix[i][j-1] = 2*matrix[i][j-1];
-                        matrix[i][j] = 0;
-
-                        merged[i][j-1] = true;
-                        merged[i][j] = false;
-                    }
-                    // if left cell already merged do nothing
+                    merged[i][j-1] = true;
+                    merged[i][j] = false;
                 }
                 else if matrix[i][j-1] == 0 {
                     matrix[i][j-1] = matrix[i][j];
                     matrix[i][j] = 0;
 
-                    merged[i][j-1] = false;
+                    merged[i][j-1] = merged[i][j];
                     merged[i][j] = false;
                 }
             } 
         }
         print_matrix(*matrix);
     }
+
+    generate_tile(matrix);
+}
+
+// Movement of the game for the right arrow
+fn move_right(matrix : &mut [[u128; 4]; 4]) {
+
+    let mut merged: [ [bool; 4]; 4] = [
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false]
+    ];
+
+    for k in 0..3 {
+        for j in (k..3).rev() {
+            for i in 0..4 {
+                if (matrix[i][j] == matrix[i][j+1]) && (!merged[i][j+1]) {
+                    matrix[i][j+1] *= 2;
+                    matrix[i][j] = 0;
+
+                    merged[i][j+1] = true;
+                    merged[i][j] = false;
+                }
+                else if matrix[i][j+1] == 0 {
+                    matrix[i][j+1] = matrix[i][j];
+                    matrix[i][j] = 0;
+
+                    merged[i][j+1] = merged[i][j];
+                    merged[i][j] = false;
+                }
+            } 
+        }
+        print_matrix(*matrix);
+    }
+
+    generate_tile(matrix);
+}
+
+// Movement of the game for the up arrow
+fn move_up(matrix : &mut [[u128; 4]; 4]) {
+
+    let mut merged: [ [bool; 4]; 4] = [
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false]
+    ];
+
+    for k in 0..3 {
+        for i in 1..(4-k) {
+            for j in 0..4 {
+                if (matrix[i][j] == matrix[i-1][j]) && (!merged[i-1][j]) {
+                    matrix[i-1][j] *= 2;
+                    matrix[i][j] = 0;
+
+                    merged[i-1][j] = true;
+                    merged[i][j] = false;
+                }
+                else if matrix[i-1][j] == 0 {
+                    matrix[i-1][j] = matrix[i][j];
+                    matrix[i][j] = 0;
+
+                    merged[i-1][j] = merged[i][j];
+                    merged[i][j] = false;
+                }
+            } 
+        }
+        print_matrix(*matrix);
+    }
+
+    generate_tile(matrix);
+}
+
+fn move_down(matrix : &mut [[u128; 4]; 4]) {
+
+    let mut merged: [ [bool; 4]; 4] = [
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false]
+    ];
+
+    for k in 0..3 {
+        for i in (k..3).rev() {
+            for j in 0..4 {
+                if (matrix[i][j] == matrix[i+1][j]) && (!merged[i+1][j]) {
+                    matrix[i+1][j] *= 2;
+                    matrix[i][j] = 0;
+
+                    merged[i+1][j] = true;
+                    merged[i][j] = false;
+                }
+                else if matrix[i+1][j] == 0 {
+                    matrix[i+1][j] = matrix[i][j];
+                    matrix[i][j] = 0;
+
+                    merged[i+1][j] = merged[i][j];
+                    merged[i][j] = false;
+                }
+            } 
+        }
+        print_matrix(*matrix);
+    }
+
+    generate_tile(matrix);
 }
 
 fn main() {
     //yew::Renderer::<App>::new().render();
     let mut matrix: [ [u128; 4] ; 4] = [
-        [4, 0, 4, 0],
-        [0, 2, 0, 0],
-        [0, 2, 0, 2],
-        [2, 2, 2, 2]
+        [4, 8, 4, 8],
+        [8, 2, 8, 8],
+        [8, 2, 8, 2],
+        [2, 2, 8, 2]
     ];
-    print_matrix(matrix);
-    transverse(&mut matrix, [0, 0]);
-    print_matrix(matrix);
+    generate_tile(&mut matrix);
+
+    // TODO create function to check if a movement is valid, aka, do something, so that it can be moved and generate a new block.
+
+    // todo check function
 }   
