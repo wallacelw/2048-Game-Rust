be089efd46c5793ea36c538d7e9266482035997f
wallacelw <70167391+wallacelw@users.noreply.github.com>
2023-01-31 19:08:15 -0300

    refactoring movement, changed to shift()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use rand::Rng;
 use std::io;
+use std:: mem;
 
 // Cria o html
 // Mas por enquanto o jogo não usa esse front
@@ -9,6 +10,17 @@ fn app() -> Html {
     html! {
         <h1>{ "Hello World" }</h1>
     }
+}
+
+// temporary debugging function
+fn print_matrix(matrix: [[u128; 4]; 4]) {
+    for i in 0..4 {
+        for j in 0..4 {
+            print!("{} ", matrix[i][j]);
+        }
+        println!();
+    }
+    println!();
 }
 
 /*
@@ -93,20 +105,51 @@ fn generate_tile(matrix : &mut [[u128; 4]; 4]) -> bool {
     return false;
 }
 
-// temporary debugging function
-fn print_matrix(matrix : [[u128; 4]; 4]) {
-    for i in 0..4 {
-        for j in 0..4 {
-            print!("{} ", matrix[i][j]);
-        }
-        println!();
+/* Função de movimentação
+Retorna se o jogo acabou:
+    true -> acabou
+    false -> não acabou ainda
+Direction[] define a orientação da movimentação:
+    [0,  1] -> para a direita
+    [0, -1] -> para a esquerda
+    [ 1, 0] -> para baixo
+    [-1, 0] -> para cima
+*/
+fn shift_aux(matrix: &mut [[u128; 4]; 4], merged: &mut [ [bool; 4]; 4], direction: [i32; 2], i: i32, j:i32, null_movement: &mut bool) {
+    let mut row = i as usize;
+    let mut col = j as usize;
+
+    // change horizontal movement to vertical movement
+    if direction[0] != 0 {
+        mem::swap(&mut row, &mut col);
+    };
+
+    let adj_row = (row as i32 + direction[0]) as usize;
+    let adj_col = (col as i32 + direction[1]) as usize;
+
+    if matrix[row][col] == 0 {return};
+
+    if (matrix[row][col] == matrix[adj_row][adj_col]) && 
+    (!merged[adj_row][adj_col]) && (!merged[row][col]) {
+        matrix[adj_row][adj_col] *= 2;
+        matrix[row][col] = 0;
+        
+        merged[adj_row][adj_col] = true;
+        merged[row][col] = false;
+        *null_movement = false;
     }
-    println!();
+
+    else if matrix[adj_row][adj_col] == 0 {
+        matrix[adj_row][adj_col] = matrix[row][col];
+        matrix[row][col] = 0;
+
+        merged[adj_row][adj_col] = merged[row][col];
+        merged[row][col] = false;
+        *null_movement = false;
+    }
 }
 
-// Função de movimentação para esquerda
-// Retorna se o jogo acabou
-fn move_left(matrix : &mut [[u128; 4]; 4]) -> bool {
+fn shift(matrix : &mut [[u128; 4]; 4], direction : [i32; 2]) -> bool {
 
     let mut merged: [ [bool; 4]; 4] = [
         [false, false, false, false],
@@ -118,159 +161,24 @@ fn move_left(matrix : &mut [[u128; 4]; 4]) -> bool {
     let mut null_movement: bool = true;
 
     for k in 0..3 {
-        for j in 1..(4-k) {
-            for i in 0..4 {
-                if matrix[i][j] == 0 {continue};
-                
-                if (matrix[i][j] == matrix[i][j-1]) && (!merged[i][j-1]) && (!merged[i][j]) {
-                    matrix[i][j-1] *= 2;
-                    matrix[i][j] = 0;
-
-                    merged[i][j-1] = true;
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-                else if matrix[i][j-1] == 0 {
-                    matrix[i][j-1] = matrix[i][j];
-                    matrix[i][j] = 0;
-
-                    merged[i][j-1] = merged[i][j];
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-            } 
+        // left, up
+        if (direction[0] + direction[1]) == -1 {
+            for j in 1..(4-k) {
+                for i in 0..4 {
+                    shift_aux(matrix, &mut merged, direction, i, j, &mut null_movement);
+                } 
+            }
         }
-        print_matrix(*matrix); // <- remove this after front
-    }
-    if null_movement {return false};
 
-    return generate_tile(matrix);
-}
-
-// Função de movimentação para direita
-// Retorna se o jogo acabou
-fn move_right(matrix : &mut [[u128; 4]; 4]) -> bool {
-
-    let mut merged: [ [bool; 4]; 4] = [
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false]
-    ];
-
-    let mut null_movement: bool = true;
-
-    for k in 0..3 {
-        for j in (k..3).rev() {
-            for i in 0..4 {
-                if matrix[i][j] == 0 {continue};
-                
-                if (matrix[i][j] == matrix[i][j+1]) && (!merged[i][j+1]) && (!merged[i][j]) {
-                    matrix[i][j+1] *= 2;
-                    matrix[i][j] = 0;
-
-                    merged[i][j+1] = true;
-                    merged[i][j] = false;
-                    null_movement = false;
+        // right, down
+        else {
+            for j in (k..3).rev() {
+                for i in 0..4 {
+                    shift_aux(matrix, &mut merged, direction, i, j, &mut null_movement);
                 }
-                else if matrix[i][j+1] == 0 {
-                    matrix[i][j+1] = matrix[i][j];
-                    matrix[i][j] = 0;
-
-                    merged[i][j+1] = merged[i][j];
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-            } 
+            }
         }
-        print_matrix(*matrix); // <- remove this after front
-    }
 
-    if null_movement {return false};
-
-    return generate_tile(matrix);
-}
-
-// Função de movimentação para cima
-// Retorna se o jogo acabou
-fn move_up(matrix : &mut [[u128; 4]; 4]) -> bool {
-
-    let mut merged: [ [bool; 4]; 4] = [
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false]
-    ];
-
-    let mut null_movement: bool = true;
-
-    for k in 0..3 {
-        for i in 1..(4-k) {
-            for j in 0..4 {
-                if matrix[i][j] == 0 {continue};
-                
-                if (matrix[i][j] == matrix[i-1][j]) && (!merged[i-1][j]) && (!merged[i][j]) {
-                    matrix[i-1][j] *= 2;
-                    matrix[i][j] = 0;
-
-                    merged[i-1][j] = true;
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-                else if matrix[i-1][j] == 0 {
-                    matrix[i-1][j] = matrix[i][j];
-                    matrix[i][j] = 0;
-
-                    merged[i-1][j] = merged[i][j];
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-            } 
-        }
-        print_matrix(*matrix); // <- remove this after front
-    }
-
-    if null_movement {return false};
-
-    return generate_tile(matrix);
-}
-
-// Função de movimentação para baixo
-// Retorna se o jogo acabou
-fn move_down(matrix : &mut [[u128; 4]; 4]) -> bool {
-
-    let mut merged: [ [bool; 4]; 4] = [
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false],
-        [false, false, false, false]
-    ];
-
-    let mut null_movement: bool = true;
-
-    for k in 0..3 {
-        for i in (k..3).rev() {
-            for j in 0..4 {
-                if matrix[i][j] == 0 {continue};
-                
-                if (matrix[i][j] == matrix[i+1][j]) && (!merged[i+1][j]) && (!merged[i][j]) {
-                    matrix[i+1][j] *= 2;
-                    matrix[i][j] = 0;
-
-                    merged[i+1][j] = true;
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-                else if matrix[i+1][j] == 0 {
-                    matrix[i+1][j] = matrix[i][j];
-                    matrix[i][j] = 0;
-
-                    merged[i+1][j] = merged[i][j];
-                    merged[i][j] = false;
-                    null_movement = false;
-                }
-            } 
-        }
         print_matrix(*matrix); // <- remove this after front
     }
 
@@ -311,17 +219,17 @@ fn start_game(matrix : &mut [[u128; 4]; 4]) {
             Err(_) => continue,
         };
     
-        if movement == 4 {
-            if move_left(matrix) {break};
+        if movement == 4 { // Left
+            if shift(matrix, [0, -1]) {break};
         }
-        else if movement == 6 {
-            if move_right(matrix) {break};
+        else if movement == 6 { // Right
+            if shift(matrix, [0,  1]) {break};
         }
-        else if movement == 8 {
-            if move_up(matrix) {break};
+        else if movement == 8 { // Up
+            if shift(matrix, [-1, 0]) {break};
         }
-        else if movement == 2 {
-            if move_down(matrix) {break};
+        else if movement == 2 { // Down
+            if shift(matrix, [ 1, 0]) {break};
         }
         else {break};
 
